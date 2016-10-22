ab301fc55d810783aeda27a27b817ac23e2c88e6
marcelbelledin <marcelbelledin@o2online.de>
2016-10-22 11:00:41 +0200

    change wiced pins and add resolution

--- Wiced_OpenAir/Wiced_OpenAir.ino
+++ Wiced_OpenAir/Wiced_OpenAir.ino
@@ -1,17 +1,19 @@
 #include "OpenAirSensor/OpenAirSensor.h"
 // config lines - if you are using it on feather M0/Wiced
 #ifdef ARDUINO_STM32_FEATHER 
-  #define en PC3
-  #define pre PC2
-  #define vred PA3
-  #define vnox PA2
+  #define en WAKE //or PA0
+  #define pre PC3
+  #define vnox PA3
+  #define vred PC2
   #define in_type INPUT_ANALOG
+  #define resolution 4096
 #elif defined(ARDUINO_SAMD_FEATHER_M0)
   #define en A0
   #define pre A1
   #define vred A2
   #define vnox A3
   #define in_type INPUT
+  #define resolution 1024
 #endif
   
 OpenAirSensor sensor(en,pre,vred,vnox);
@@ -21,7 +23,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(vnox, in_type);
   pinMode(vred, in_type);
-  //sensor.change_resolution(4096); change to your current resolution steps of the ADC;
+  sensor.change_resolution(resolution); //change to your current resolution steps of the ADC;
 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -29,7 +31,11 @@ void setup() {
 }
 
 void loop() {
+  delay(10000);
+  digitalWrite(pre, 1);
+  delay(30000);
+  digitalWrite(pre, 0);
+  
   Serial.println(sensor.NO2_ppm());
-  Serial.println(sensor.r_ox); // get the calculated resistance
-  delay(1000);
+  Serial.println(sensor.CO_ppm());
 }
